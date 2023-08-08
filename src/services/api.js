33e3d9953ebec3e2d164690319264894dd3ef514
VIJAI SURIA <98590895+vijaisuria@users.noreply.github.com>
2023-08-09 02:27:35 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Replace with your actual API base URL
+  baseURL: "https://helth-center-api.onrender.com/api", // Replace with your actual API base URL
 });
 
 // Add a response interceptor to handle server errors
