6e5c686c6b297dda37e37163f2d328299a2100fd
VIJAI SURIA <98590895+vijaisuria@users.noreply.github.com>
2023-08-09 03:01:50 +0530

    Update nurseProfile .js

--- src/components/profile/nurseProfile .js
+++ src/components/profile/nurseProfile .js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../../services/api";
 import { useNavigate } from "react-router-dom";
 
 import { ToastContainer, toast } from "react-toastify";
