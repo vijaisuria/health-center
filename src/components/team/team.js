188a2ad3a41c6f17076ce1201f3ea8647272b793
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2023-09-19 13:18:23 +0530

    team details updated

--- src/components/team/team.js
+++ src/components/team/team.js
@@ -291,7 +291,7 @@ const Team = () => {
                 </div>
               </div>
             </div>
-
+            {/*
             <div
               role="listitem"
               className="xl:w-1/3 lg:mx-3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5"
@@ -377,6 +377,7 @@ const Team = () => {
                 </div>
               </div>
             </div>
+            */}
           </div>
         </div>
       </div>
